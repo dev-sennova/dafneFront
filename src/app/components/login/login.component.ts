008c7a5b1261a850066946651d377dba0122a77d
Darwing27 <dsrondon96@misena.edu.co>
2024-05-03 17:25:49 -0500

    DAFNEFRONT-4-Implementacion_vistas_verificacion_correo. Se modifican las vistas, implementando los cambios realizados en el backend, permitiendo ahora la verificacion de los correos registrados.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { Router } from '@angular/router';
+import { error } from 'jquery';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
@@ -10,23 +13,91 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   usuario: string;
   password: string;
+  Viewpin: boolean = false;
+  pin: string;
+  message!: string;
+
 
   constructor(public loginService: LoginService, public router: Router) {
-    this.usuario="";
-    this.password="";
+    this.usuario = "";
+    this.password = "";
+    this.pin = "";
   }
 
-  login(){
-    const user = {email:this.usuario, password:this.password};
-    this.loginService.signIn(user);
+  login() {
+    const user = { email: this.usuario, password: this.password };
+    this.loginService.signIn(user).subscribe(
+      (response) => {
+        localStorage.setItem('access_token', response.access_token);
+        localStorage.setItem('nombre_usuario', response.nombre_usuario);
+        localStorage.setItem('email_usuario', response.email_usuario);
+        localStorage.setItem('identificador_usuario', response.id_usuario);
+        localStorage.setItem('rol', response.rol);
+
+        // Redireccionar según el rol
+        if (response.rol == 1) {
+          this.router.navigate(['administrador']);
+        } else if (response.rol == 3) {
+          this.router.navigate(['home']);
+        }
+      },
+      (error) => {
+        if (error.error.error_code == 'email_not_verified') {
+          this.Viewpin = true;
+        }
+      }
+    );
   }
 
-  loginFake(){
+  verifyEmail() {
+    this.loginService.verifyEmail(this.usuario, this.pin).subscribe(
+      (response) => {
+        localStorage.setItem('access_token', response.access_token);
+        localStorage.setItem('nombre_usuario', response.nombre_usuario);
+        localStorage.setItem('email_usuario', response.email_usuario);
+        localStorage.setItem('identificador_usuario', response.id_usuario);
+        localStorage.setItem('rol', response.rol);
+
+        // Redireccionar según el rol
+        if (response.rol == 1) {
+          this.router.navigate(['administrador']);
+        } else if (response.rol == 3) {
+          this.router.navigate(['home']);
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  resendEmail() {
+    this.loginService.resendEmail(this.usuario).subscribe(
+      (response)=>{
+        Swal.fire({
+          title: "Reenvio de correo",
+          text: "Se ha reenviado correctamente el email al correo electrónico registrado",
+          icon: "success"
+        });
+      },
+      (error)=>{
+        Swal.fire({
+          title: "Error en el reenvio",
+          text: "No se pudo enviar el email, intente más tarde o verifique si la dirección es válida.",
+          icon: "error"
+        });
+        console.log('Error al reenviar el correo:',error);
+      }
+    )
+  }
+
+
+  loginFake() {
     this.router.navigate(['home']);
   }
 
   ngOnInit(): void {
-    console.log("Valor isLoggedIn: "+this.loginService.isLoggedIn);
+    console.log("Valor isLoggedIn: " + this.loginService.isLoggedIn);
   }
 
 }
--- src/app/interceptors/session.interceptor.ts
+++ src/app/interceptors/session.interceptor.ts
@@ -13,11 +13,17 @@ export class SessionInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
-        if (err.status === 401) {
+        if (err.status === 401 && err.error.error_code != 'email_not_verified') {
             // auto logout if 401 response returned from api
             this.authenticationService.doLogout();
             Swal.fire('No pudo autenticarse, por favor ingrese nuevamente')
             this.router.navigate(['login']);
+        }else if(err.status === 401 && err.error.error_code == 'email_not_verified') {
+            Swal.fire({
+                title: "¿Primera vez por acá?",
+                text: "Bienvenido, hemos enviado un email con un codigo a tu correo para comprobar que eres tú. Este es el ultimo paso para poder usar D.A.F.N.E",
+                icon: "info"
+              });
         }
         //        const error = "Sesión expirada, por favor ingrese nuevamente";
         //        const error = err.status;
